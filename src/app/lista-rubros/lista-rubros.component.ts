624fb50be01bfe0e3e7c71167481cada1a58be66
DarwinC <darwin.carrizo@gmail.com>
2020-09-20 12:23:59 -0300

    3 files modified

--- src/app/lista-rubros/lista-rubros.component.ts
+++ src/app/lista-rubros/lista-rubros.component.ts
@@ -7,12 +7,24 @@ import { RubrosService } from '../services/rubros.service';
   styleUrls: ['./lista-rubros.component.css']
 })
 export class ListaRubrosComponent implements OnInit {
-
+rubros;
   constructor(
     private rubrosService: RubrosService,
   ) { }
 
   ngOnInit() {
+
+  this.rubrosService.getAll().subscribe(a => {
+      console.log(a);
+      this.rubros = a['rubros'];
+      console.log("Se consulto el listado de rubros");
+    },
+    err => {
+      if(err.status === 500){
+        console.log("Ha ocurrido un error en el servidor");
+      }
+    });
+
   }
 
 }
