a841087c629c6630cb089a8fbedce587cde21a8f
DarwinC <darwin.carrizo@gmail.com>
2020-09-22 01:20:13 -0300

    5 files modified, 3 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from "@angular/core";
+import { NgModule,CUSTOM_ELEMENTS_SCHEMA } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
 
 import { ReactiveFormsModule, FormsModule } from "@angular/forms";
@@ -13,6 +13,7 @@ import { RegistroComponent } from './registro/registro.component';
 import { LoginComponent } from './login/login.component';
 import { ListaRubrosComponent } from './lista-rubros/lista-rubros.component';
 import { AddGastoComponent } from './add-gasto/add-gasto.component';
+import { ListaGastos2Component } from './lista-gastos2/lista-gastos2.component';
 
 
 
@@ -26,13 +27,18 @@ import { AddGastoComponent } from './add-gasto/add-gasto.component';
       { path: "", component:  LoginComponent},
       { path: "login", component:  LoginComponent},
       { path: "gastos", component:  ListaGastosComponent},
+      { path: "gastos2", component:  ListaGastos2Component},
       { path: "gastos/agregar-gasto", component:  AddGastoComponent},
       { path: "rubros", component:  ListaRubrosComponent},
       { path: "registro", component:  RegistroComponent},
 
     ])
+
+],
+    schemas: [
+  CUSTOM_ELEMENTS_SCHEMA
   ],
-  declarations: [AppComponent, TopBarComponent, ListaGastosComponent, FooterComponent, RegistroComponent, LoginComponent, ListaRubrosComponent, AddGastoComponent],
+  declarations: [AppComponent, TopBarComponent, ListaGastosComponent, FooterComponent, RegistroComponent, LoginComponent, ListaRubrosComponent, AddGastoComponent, ListaGastos2Component],
   bootstrap: [AppComponent],
 
 })
